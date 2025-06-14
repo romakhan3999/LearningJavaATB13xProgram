d97622e83cb0ed6e65c416387ad976a64a85090a
romakhan3999 <khanroma02@gmail.com>
2025-06-14 16:53:12 +0530

    14th june task 2025

---
+++ src/ex_03_Literals/Task_10_June_Table9.java
@@ -0,0 +1,18 @@
+package ex_03_Literals;
+
+public class Task_10_June_Table9 {
+    public static void main(String[] args) {
+        int a =9;
+        System.out.printf("The Table of %d is given below \n",a);
+        System.out.printf("%d x 1 = %d \n",a,(a*1));
+        System.out.printf("%d x 2 = %d\n",a,(a*2));
+        System.out.printf("%d x 3 = %d\n",a,(a*3));
+        System.out.printf("%d x 4 = %d\n",a,(a*4));
+        System.out.printf("%d x 5 = %d\n",a,(a*5));
+        System.out.printf("%d x 6 = %d\n",a,(a*6));
+        System.out.printf("%d x 7 = %d\n",a,(a*7));
+        System.out.printf("%d x 8 = %d\n",a,(a*8));
+        System.out.printf("%d x 9 = %d\n",a,(a*9));
+        System.out.printf("%d x 10 = %d\n",a,(a*10));
+    }
+}
